f069fa9645f5fba88ee074efbab444a1583a205f
jonasjohnson1@gmail.com <jonasjohnson1@gmail.com>
2016-10-14 16:15:18 +0000

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,11 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    end
+    
     private
       def article_params
           params.require(:article).permit(:title, :description)
