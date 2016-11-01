01c2eef7541be377c7050f15a65487e82666af31
jonasjohnson1@gmail.com <jonasjohnson1@gmail.com>
2016-11-01 18:54:45 +0000

    Updated partials for error and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:sucess] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     def update
         
         if @article.update(article_params)
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article)
         else
             render 'edit'
